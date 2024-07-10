07cb99824ed98f7de05e0c86dfccc7a583ecd446
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-07-10 23:56:13 +0530

    subscribe() -> it is equivalent of forEach in reactive read() is added at end because if we don't add it, program will end before printing any number because stream is waiting for events(1s delay has been added in stream)

--- src/main/java/org/shiv/Exercise2.java
+++ src/main/java/org/shiv/Exercise2.java
@@ -10,9 +10,15 @@ public class Exercise2 {
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
         // TODO: Write code here
+        System.out.println("Print all numbers in the ReactiveSources.intNumbersFlux stream");
+        ReactiveSources.intNumbersFlux()
+                .subscribe(element -> System.out.print(element+" , "));
 
         // Print all users in the ReactiveSources.userFlux stream
         // TODO: Write code here
+        System.out.println("Print all users in the ReactiveSources.userFlux stream");
+        ReactiveSources.userFlux()
+                        .subscribe(user -> System.out.print(user+" , "));
 
         System.out.println("Press a key to end");
         System.in.read();
