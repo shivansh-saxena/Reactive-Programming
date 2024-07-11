072c6e23a5373926c4f6357fe830a2d6d578c55a
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-07-12 00:10:15 +0530

    -Mono is 0 or 1 item(can have only one item) it's asynchronous -after subscribe() on a mono we know there is no more data coming from publisher -block() can give the value or null depending on source -blockOptional() returns a Optional instead of original object, so we can handel null.

--- src/main/java/org/shiv/Exercise4.java
+++ src/main/java/org/shiv/Exercise4.java
@@ -1,6 +1,7 @@
 package org.shiv;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class Exercise4 {
 
@@ -10,9 +11,22 @@ public class Exercise4 {
 
         // Print the value from intNumberMono when it emits
         // TODO: Write code here
+        System.out.println("Print the value from intNumberMono when it emits");
+
+        ReactiveSources.intNumberMono()
+                .subscribe(number -> System.out.println(number));
 
         // Get the value from the Mono into an integer variable
         // TODO: Write code here
+        System.out.println("Get the value from the Mono into an integer variable");
+
+        Integer number = ReactiveSources.intNumberMono()
+                        .block();
+        System.out.println(number);
+
+        Optional<Integer> numberOptional = ReactiveSources.intNumberMono()
+                .blockOptional();
+        System.out.println(numberOptional.orElse(-1));
 
         System.out.println("Press a key to end");
         System.in.read();
